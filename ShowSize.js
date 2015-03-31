b97f35559cae3cfc45024b7b6ecb4d65e4006cca
VictoriaSnowfalke <JaguarBenz@yandex.ru>
2015-03-31 20:55:34 +0600

    Hide and Show
    Ability to hide and show ShowSize line. Just click on triange to hide or
    on SS block to show.

--- ShowSize.js
+++ ShowSize.js
@@ -7,3 +7,20 @@ function ShowSize(){
 	var x=document.documentElement.clientWidth;
 	document.getElementById('number').innerHTML =x;
 }
+var elem = document.getElementsByClassName("treyg");
+for (var i=0;i<elem.length;i++){
+	elem[i].addEventListener("click", myFunction1);
+}
+
+function myFunction1() {
+    document.getElementById("adjustment").style.display = "none";
+    document.getElementById("SS").style.display = "inline-block";
+}
+
+document.getElementById("SS").addEventListener("click", myFunction2);
+
+
+function myFunction2() {
+    document.getElementById("adjustment").style.display = "block";
+    document.getElementById("SS").style.display = "none";
+}
